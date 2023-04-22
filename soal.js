93748f29af4771d73490623159979bc3936cc3ed
carlosmaruao <87672851+carlosmaruao@users.noreply.github.com>
2023-04-22 15:38:30 +0700

    Add files via upload

---
+++ soal.js
@@ -0,0 +1,224 @@
+export const dataSoal = [
+  // {
+  //   id: 4,
+  // pelajaran: "Matematika",
+  //   question: "qwqwe",
+  //   answers: [
+  //     {
+  //       text: "aaaaa",
+  //       image: "",
+  //       correct: false,
+  //     },
+  //     {
+  //       text: "aaaaa",
+  //       image: "",
+  //       correct: false,
+  //     },
+  //     {
+  //       text: "aaaaa",
+  //       image: "",
+  //       correct: false,
+  //     },
+  //     {
+  //       text: "aaaaa",
+  //       image: "",
+  //       correct: false,
+  //     },
+  //   ],
+  // },
+  {
+    id: 7,
+    pelajaran: "Matematika",
+    question:
+      "-24 x 99 = (-24 x 100) – (-24 x n), Nilai yang tepat untuk n adalah ...",
+    answers: [
+      {
+        text: "-99",
+        image: "",
+        correct: false,
+      },
+      {
+        text: "0",
+        image: "",
+        correct: true,
+      },
+      {
+        text: "1",
+        image: "",
+        correct: false,
+      },
+      {
+        text: "99",
+        image: "",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 6,
+    pelajaran: "Matematika",
+    question: "Sifat asosiatif ditunjukkan pada persamaan ...",
+    answers: [
+      {
+        text: "(3+6) + 4 = (6+4) + 3",
+        image: "",
+        correct: false,
+      },
+      {
+        text: "3 + (6+4) = 3 + (4+6)",
+        image: "",
+        correct: false,
+      },
+      {
+        text: "(3+6) + 4 = 3 + (6+4)",
+        image: "",
+        correct: true,
+      },
+      {
+        text: "6+4) + 3 = (4+6) + 3",
+        image: "",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 5,
+    pelajaran: "Bahasa Indonesia",
+    question:
+      "<code>Supaya kondisi badan tetap sehat, sebaiknya masyarakat tetap bisa beraktivitas secara aktif dalam sehari-hari.</code>Dalam kalimat tersebut, makna dari istilah beraktivitas yang digunakan yaitu…",
+    answers: [
+      {
+        text: "Belajar",
+        image: "",
+        correct: false,
+      },
+      {
+        text: "Bekerja",
+        image: "",
+        correct: false,
+      },
+      {
+        text: "Berolahraga",
+        image: "",
+        correct: false,
+      },
+      {
+        text: "Berkegiatan",
+        image: "",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 4,
+    pelajaran: "Bahasa Indonesia",
+    question:
+      "<code>Kedisiplinan lalu lintas yang dimiliki oleh masyarakat Madiun cenderung menurun. Hal ini terbukti akan bertambahnya angka pelanggaran yang tercatat dalam data kepolisian. Selain itu, jumlah korban yang meninggal akan kejadian kecelakaan pun juga semakin meningkat.</code>Paragraf di atas bisa diidentifikasikan sebagai jenis teks yaitu…",
+    answers: [
+      {
+        text: "Deskripsi",
+        image: "",
+        correct: false,
+      },
+      {
+        text: "Argumentasi",
+        image: "",
+        correct: false,
+      },
+      {
+        text: "Eksposisi",
+        image: "",
+        correct: false,
+      },
+      {
+        text: "Narasi",
+        image: "",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 3,
+    pelajaran: "Bahasa Indonesia",
+    question:
+      "Tim SAR sedang <i>mengevakuasi</i> para korban bencana banjir di Bogor dengan memanfaatkan penggunaan perahu karet. <br>Makna yang dimiliki pada kata yang dicetak secara miring dalam kalimat di atas yaitu…",
+    answers: [
+      {
+        text: "Menyelamatkan",
+        image: "",
+        correct: false,
+      },
+      {
+        text: "Memindahkan",
+        image: "",
+        correct: true,
+      },
+      {
+        text: "Menyeberangkan",
+        image: "",
+        correct: false,
+      },
+      {
+        text: "Menolong",
+        image: "",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    pelajaran: "Bahasa Indonesia",
+    question:
+      "Simak kutipan paragraf yang ada berikut ini. <code>Lila merupakan anak dari pasangan Pak Deni dan Ibu Lita. Ia hidup dengan keluarga yang bergelut dalam bidang seni karena ibunya merupakan seorang pelukis terkenal. Sementara ayahnya yaitu penari yang memiliki studio tersendiri di rumahnya. Dalam diri Lila juga mengalir darah seorang seniman yang cukup kental. Dari kecil, Lila selalu mengikuti les piano yang merupakan arahan dari orangtuanya. Hal ini dilakukan pula dari kecil karena Lila selalu menyukai seni musik dibandingkan seni tari maupun seni lukis. Harapan kedua orangtuanya yaitu agar Lila kelak dapat menjadi seorang pianis yang terkenal.</code>Dari kutipan paragraf tersebut, yang menjadi kalimat utama yaitu…",
+    answers: [
+      {
+        text: "Ibunya adalah seorang pelukis terkenal",
+        image: "",
+        correct: false,
+      },
+      {
+        text: "Dari kecil, Lila selalu mengikuti les piano yang merupakan arahan dari orangtuanya",
+        image: "",
+        correct: true,
+      },
+      {
+        text: "Lila merupakan anak dari pasangan Pak Deni dan Ibu Lita",
+        image: "",
+        correct: false,
+      },
+      {
+        text: "Harapan kedua orangtuanya yaitu agar Lila kelk dapat menjadi seorang pianis yang terkenal",
+        image: "",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 1,
+    pelajaran: "Bahasa Indonesia",
+    question:
+      "Dani merupakan  murid  kelas 6 dan seringkali menjadi contoh anak yang <i>patuh</i> terhadap kedua orangtuanya. <br>Antonim dari kata <i>patuh</i> yang digunakan dalam kalimat di atas yaitu…",
+    answers: [
+      {
+        text: "Nakal",
+        image: "",
+        correct: true,
+      },
+      {
+        text: "Menghormati",
+        image: "",
+        correct: false,
+      },
+      {
+        text: "Melanggar",
+        image: "",
+        correct: false,
+      },
+      {
+        text: "Berbakti",
+        image: "",
+        correct: false,
+      },
+    ],
+  },
+];
